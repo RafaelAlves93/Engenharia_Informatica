89d4b01da48ac060414c792bafa4637792a416f5
[Rafael Alves] <[rafaalves0793@gmail.com]>
2023-06-02 22:02:20 +0100

    guiao2

---
+++ P/Guiao2/1/ponto.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include "ponto.h"
+
+// alinea b)
+void printPonto(ponto2D a){
+    printf("Ponto: (%d,%d)\n", a.x, a.y);
+}
+
+// alinea c)
+void initPonto(ponto2D* p){
+    // completar a função
+}
+
+// alinea d)
+void movePonto(ponto2D* p, int dx, int dy){
+    // completar a função
+}
